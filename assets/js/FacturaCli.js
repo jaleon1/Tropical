eedac09cecd62b781740efcd8fcb2a833edfef2d
jaleon1 <jairolg27@hotmail.com>
2018-07-26 19:38:05 -0600

    Impresion

--- assets/js/FacturaCli.js
+++ assets/js/FacturaCli.js
@@ -12,6 +12,19 @@ class FacturaCli {
         // this.importe = importe || 0;
         // this.t = t || null;
     }
+
+    ticketPrint(e){
+        var data = JSON.parse(e);
+        localStorage.setItem("lsFactura",data.consecutivo);
+        localStorage.setItem("lsFecha",data.fechaCreacion);
+        localStorage.setItem("lsBodega",data.local);
+        localStorage.setItem("lsUsuario",data.terminal);
+        localStorage.setItem("lsSubTotal",data.totalComprobante);
+        localStorage.setItem("lsTotal",data.totalComprobante);
+        localStorage.setItem("lsListaProducto",JSON.stringify(data.detalleFactura));
+    
+        location.href ="/Tropical/TicketFacturacion.html";
+    }
 }
 
 let facturaCli = new FacturaCli();
@@ -888,19 +901,6 @@ function alertFact() {
     $(".procesarFac").prop('disabled', true);
     // calcVuelto();
     setTimeout(function () { location.reload(); }, 2000);
-}
-
-function ticketPrint(e){
-    var data = JSON.parse(e);
-    localStorage.setItem("lsFactura",data.consecutivo);
-    localStorage.setItem("lsFecha",data.fechaCreacion);
-    localStorage.setItem("lsBodega",data.local);
-    localStorage.setItem("lsUsuario",data.terminal);
-    localStorage.setItem("lsSubTotal",data.totalComprobante);
-    localStorage.setItem("lsTotal",data.totalComprobante);
-    localStorage.setItem("lsListaProducto",JSON.stringify(data.lista));
-
-    location.href ="/Tropical/TicketFacturacion.html";
 }
 
 function calcVuelto(pago, xPagar) {
