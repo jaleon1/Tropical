0ec837103433ee772bc67ebd8b2addc316da8c0c
jaroja4 <jaroja4@gmail.com>
2018-07-26 23:32:50 -0600

    solucionado impresion + where de productos en faccli

--- class/Producto.php
+++ class/Producto.php
@@ -118,7 +118,8 @@ class Producto{
             $sql='SELECT ib.id, p.codigo, p.nombre, p.txtColor, p.bgColor, p.nombreAbreviado, p.descripcion, ib.saldoCantidad, p.esVenta
             FROM insumosXBodega as ib
             INNER JOIN  producto as p on p.id = ib.idProducto
-            WHERE esVenta=1 or esVenta=2
+            WHERE (esVenta=1 or esVenta=2)
+            and ib.idBodega = :idBodega
             ORDER BY p.nombre';
             $param= array(':idBodega'=>$_SESSION["userSession"]->idBodega);
             $data= DATA::Ejecutar($sql,$param);
