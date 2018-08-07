a462c40915dca3fce0f101fa515bb3befcccdccd
jaroja4 <jaroja4@gmail.com>
2018-08-07 15:45:33 -0600

    cambios menores 2.0

--- class/Globals.php
+++ class/Globals.php
@@ -4,17 +4,5 @@ class Globals {
     const version= '1.0';
     const cssversion= "1.0";
     static $config="";    
-    
-    // public static function ConfiguracionIni(){     
-    //     if (file_exists('../../ini/config.ini')) {
-    //         self::$config = parse_ini_file('../../ini/config.ini',true); 
-    //     } 
-    //     else if (file_exists('../ini/config.ini')) {
-    //         self::$config = parse_ini_file('../ini/config.ini',true); 
-    //     }
-    //     else if (file_exists('../../../ini/config.ini')) {
-    //         self::$config = parse_ini_file('../../../ini/config.ini',true); 
-    //     }   
-    // }  
 }
 ?>
