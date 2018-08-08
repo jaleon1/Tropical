a6c0b4266fb961df98d810d19f6a07525e4dce5e
jaleon1 <jairolg27@hotmail.com>
2018-08-07 18:34:06 -0600

    SwalPrintOK

--- assets/js/virtualNumPad/virtualNumPad.js
+++ assets/js/virtualNumPad/virtualNumPad.js
@@ -302,17 +302,19 @@ function facturar (){
             obj: JSON.stringify(facturaCli)
         }
     })
-    .done(function(e){
-        if (e==true) {
+    .done(function(){
             swal({
                 type: 'success',
                 text: 'Factura Lista!',
-                timer: 2000
+                timer: 1000
             });    
-        }
     })
     .fail(function (e) {
-        distr.showError(e);
+        swal({
+            type: 'error',
+            text: 'Error al imprimir!',
+            timer: 1000
+        });
     })
     .always(function () {
         // location.reload();
