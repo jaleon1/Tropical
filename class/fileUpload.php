34bad938555d4e72e8d39442879d1c4af6ac5d5c
Car11 <carlos.echc11@gmail.com>
2018-08-11 15:49:01 -0600

    cert

--- class/fileUpload.php
+++ class/fileUpload.php
@@ -6,16 +6,10 @@ if (!isset($_SESSION))
     session_start();
 $uploaddir= '../../CU/'.$_SESSION['userSession']->idBodega.'/';
 if (!file_exists($uploaddir)) 
-    mkdir($uploaddir, 0777, true);
+    mkdir($uploaddir, 0700, true);
 $cfile= encdes::cifrar($_FILES['file']['name']);
 $uploadfile = $uploaddir . explode('::', $cfile)[0];
 if (!empty($_FILES)) {
-    //
-    $content = "abc";
-    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt","wb");
-    fwrite($fp,$content);
-    fclose($fp);
-    //
     if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
         $sql="UPDATE clienteFE 
                 SET cpath=:cpath, nkey=:nkey
