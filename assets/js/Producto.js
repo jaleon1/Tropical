598b5563c5853b58f1dad2272511b21e291437b2
Car11 <carlos.echc11@gmail.com>
2018-09-01 17:59:03 -0600

    03

--- assets/js/Producto.js
+++ assets/js/Producto.js
@@ -391,6 +391,19 @@ class Producto {
             responsive: true,
             destroy: true,
             order: [[ 1, "asc" ]],
+            language: {
+                "infoEmpty": "Sin Usuarios Registrados",
+                "emptyTable": "Sin Usuarios Registrados",
+                "search": "Buscar",
+                "zeroRecords":    "No hay resultados",
+                "lengthMenu":     "Mostrar _MENU_ registros",
+                "paginate": {
+                    "first":      "Primera",
+                    "last":       "Ultima",
+                    "next":       "Siguiente",
+                    "previous":   "Anterior"
+                }
+            },
             columnDefs: [{className: "text-right", "targets": [5]}],
             columns: [
                 {
