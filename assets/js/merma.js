0c442d124ef15d5e4938e70aa197a95251fa0689
Car11 <carlos.echc11@gmail.com>
2018-09-16 13:47:01 -0600

    lista

--- assets/js/merma.js
+++ assets/js/merma.js
@@ -9,6 +9,94 @@ class Merma {
         this.fecha = fecha || null;
     }
 
+    get Read() {
+        NProgress.start();
+        var miAccion = this.id == null ?  'ReadAll'  : 'Read';
+        if(miAccion=='ReadAll' && $('#tMerma tbody').length==0 )
+            return;
+        $.ajax({
+            type: "POST",
+            url: "class/merma.php",
+            data: {
+                action: miAccion,
+                id: this.id
+            }
+        })
+            .done(function (e) {
+                merma.Reload(e);
+            })
+            .fail(function (e) {
+                merma.showError(e);
+            })
+            .always(NProgress.done()); 
+    }
+
+    setTable(buttons=true, nPaging=10){
+        $('#tMerma').DataTable({
+            responsive: true,
+            info: false,
+            iDisplayLength: nPaging,
+            paging: false,
+            "language": {
+                "infoEmpty": "Sin Registros Ingresados",
+                "emptyTable": "Sin Registros Ingresados",
+                "search": "Buscar",
+                "zeroRecords":    "No hay resultados",
+                "lengthMenu":     "Mostrar _MENU_ registros",
+                "paginate": {
+                    "first":      "Primera",
+                    "last":       "Ultima",
+                    "next":       "Siguiente",
+                    "previous":   "Anterior"
+                }
+            },
+            columns: [
+                {
+                    title: "id",
+                    data: "id",
+                    className: "itemId",
+                    searchable: false
+                },
+                { title: "Codigo", data: "codigo" },
+                { title: "Nombre", data: "nombre" },
+                { title: "Descripción", data: "descripcion" },
+                { 
+                    title: "Cantidad", 
+                    data: "cantidad"
+//                     mRender: function () {
+//                         return '<input class="cantidad form-control" type="number" min="1" max="9999999999" step="1" style="text-align:right;" >'
+//                     }
+                },
+                { title: "Descripción", data: "descripcion" },
+                { 
+                    title: "Fecha", 
+                    data: "fecha"
+                }                
+            ]
+        });
+    };
+
+    Reload(e) {
+        if (this.id == null)
+            this.ShowAll(e);
+        else this.ShowItemData(e);
+    };
+
+    ShowAll(e) {
+        var t= $('#tMerma').DataTable();
+        t.clear();
+        t.rows.add(JSON.parse(e));
+        $('td:eq(5)').attr({ align: "right" });   
+        t.order([1, 'desc']).draw();
+        //$('.delete').click(merma.DeleteEventHandler);
+        //$( "#tMerma tbody tr" ).live("click", merma.viewType==undefined || merma.viewType==merma.tUpdate ? merma.UpdateEventHandler : merma.SelectEventHandler);
+        //
+        //$( document ).on( 'click', '.update', merma.UpdateEventHandler);
+        //$( document ).on( 'click', '#tMerma tbody tr td:not(.buttons)', merma.viewType==undefined || merma.viewType==merma.tUpdate ? merma.UpdateEventHandler : merma.SelectEventHandler);
+        // $( document ).on( 'click', '.delete', merma.DeleteEventHandler);
+        // $( document ).on( 'click', '.open', merma.OpenEventHandler);
+    };
+
     showInfo() {
         //$(".modal").css({ display: "none" });   
         swal({
