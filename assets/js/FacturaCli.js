aa9076eeaed441d2e3c1e314fceb04ff8922b808
Car11 <carlos.echc11@gmail.com>
2018-09-22 12:59:52 -0600

    fix02

--- assets/js/FacturaCli.js
+++ assets/js/FacturaCli.js
@@ -173,13 +173,17 @@ function LoadPreciosTamanos() {
         }
     })
         .done(function (e) {
-            if(JSON.parse(e).msg=='NOCONTRIB')
+            if(JSON.parse(e).msg=='NOCONTRIB'){
                swal({
                 type: 'warning',
                 title: 'Contribuyente',
                 text: 'Contribuyente no registrado para Facturación Electrónica',
                 footer: '<a href="clienteFE.html">Agregar Contribuyente</a>',
-                })
+                }).then((result) => {
+                    if (result.value) 
+                        location.href = "Dashboard.html";
+                })                
+            }
             else setPrecios(e);
         })
         .fail(function (e) {
