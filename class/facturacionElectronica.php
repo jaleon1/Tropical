beb9f1f1744251837d76c1afc267a2c38928a933
Car11 <carlos.echc11@gmail.com>
2018-12-02 23:24:25 -0600

    60s

--- class/facturacionElectronica.php
+++ class/facturacionElectronica.php
@@ -1007,7 +1007,7 @@ class FacturacionElectronica{
                 CURLOPT_RETURNTRANSFER => true,   
                 CURLOPT_VERBOSE => true,                      
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
+                CURLOPT_TIMEOUT => 60,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "POST",
                 CURLOPT_POSTFIELDS => $post
