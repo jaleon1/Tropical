2f27b6d63e950b0b0d07f155e158fe276fe9fbda
jaleon1 <jairolg27@hotmail.com>
2019-03-19 06:25:10 -0600

    listaMensajeReceptor

--- assets/js/mensajeReceptor.js
+++ assets/js/mensajeReceptor.js
@@ -1,6 +1,6 @@
 class MensajeReceptor {
     // Constructor
-    constructor(id, clave, identificacion, montoImpuesto, totalComprobante, identificacionReceptor, consecutivoFE, mensaje, detalle) {
+    constructor(id, clave, identificacion, montoImpuesto, totalComprobante, identificacionReceptor, consecutivoFE, mensaje, detalle, tb_mensajeReceptor, fechaInicial, fechaFinal) {
         this.id = id || null;
         this.clave = clave || '';
         this.identificacion = identificacion || '';
@@ -10,6 +10,9 @@ class MensajeReceptor {
         this.consecutivoFE = consecutivoFE || null;
         this.mensaje = mensaje || null;
         this.detalle = detalle || null;
+        this.tb_mensajeReceptor = tb_mensajeReceptor || null;
+        this.fechaInicial = fechaInicial || null;
+        this.fechaFinal = fechaFinal || null;
     }
 
     // get Save() {
@@ -83,6 +86,181 @@ class MensajeReceptor {
                 {
                     title: "ESTADO",
                     data: "estado"
+                }
+            ]
+        });
+    };
+
+    CargaListaMR() {
+        var rc=mr.tb_mensajeReceptor;
+        mr.tb_mensajeReceptor = [];
+        $.ajax({
+                type: "POST",
+                url: "class/mensajeReceptor.php",
+                data: {
+                    action: "ReadAllbyRange",
+                    obj: JSON.stringify(mr)
+                }
+            })
+            .done(function (e) {
+                mr.tb_mensajeReceptor = rc;
+                if (e != "null") {
+                    mr.drawMensajeReceptor(e)
+                } else {
+                    swal({
+                        type: 'success',
+                        title: 'Listo, no hay facturas que cargar!',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            })
+            .fail(function (e) {
+                mr.showError(e);
+            });
+    };
+
+    drawMensajeReceptor(e) {
+        var mesajesReceptor = JSON.parse(e);
+
+        this.tb_mensajeReceptor = $('#tb_mensajeReceptor').DataTable({
+            data: mesajesReceptor,
+            footerCallback: function (row, data, start, end, display) {
+                var api = this.api();
+                // Remove the formatting to get integer data for summation
+                var intVal = function (i) {
+                    return typeof i === 'string' ?
+                        i.replace(/[\¢,]/g, '') * 1 :
+                        typeof i === 'number' ?
+                        i : 0;
+                };
+                // Total de todas las paginas filtradas
+                var totalComprobante = display.map(el => data[el][5]).reduce((a, b) => intVal(a) + intVal(b), 0 );
+
+                // Actualiza el footer
+                $( api.column( 5 ).footer() ).html('¢' + parseFloat(Number(totalComprobante)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+            },
+            responsive: true,
+            destroy: true,
+            order: [[ 2, "desc" ]],
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {columns: [1, 2, 3, 4, 5]},                    
+                    messageTop:'Mensaje Receptor'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    orientation : 'landscape',
+                    messageTop:'Mensaje Receptor',
+                    exportOptions: {columns: [1, 2, 3, 4, 5]}
+                }
+            ],
+            language: {
+                "infoEmpty": "Sin Mensajes Receptor",
+                "emptyTable": "Sin Mensajes Receptor",
+                "search": "Buscar",
+                "zeroRecords":    "No hay resultados",
+                "lengthMenu":     "Mostrar _MENU_ registros",
+                "paginate": {
+                    "first":      "Primera",
+                    "last":       "Ultima",
+                    "next":       "Siguiente",
+                    "previous":   "Anterior"
+                }
+            },
+            columnDefs: [{className: "text-right", "targets": [4,5,7]}],
+            columns: [
+                {
+                    title:"ID",
+                    data:"id",
+                    className:"itemId",                    
+                    width:"auto",
+                    searchable: false
+                },
+                {
+                    title:"CONSECUTIVO",
+                    data:"consecutivo",
+                    width:"auto"
+                },
+                {
+                    title:"FECHA DE CREACION",
+                    data:"fechaCreacion",
+                    width:"auto"
+                },
+                {
+                    title:"DETALLE",
+                    data:"detalle",
+                    width:"auto"
+                },
+                {
+                    title:"MENSAJE",
+                    data:"mensaje",
+                    width:"auto"
+                },
+                {
+                    title:"TOTAL",
+                    data:"totalComprobante",
+                    width:"auto",
+                    type: 'formatted-num',
+                    mRender: function ( e ) {
+                        return '¢'+ parseFloat(e).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                        }
+                },
+                {
+                    title:"IDEMISOR",
+                    data:"idEmisor",
+                    width:"auto",
+                    visible: false,
+                    searchable: false
+                },
+                {
+                    title:"ESTADO",
+                    data:"idEstadoComprobante",
+                    width:"auto",
+                    mRender: function ( e ) {
+                        var tipo="1";
+                        if (e=="0") 
+                            tipo="CASO 0";
+                        if (e=="1") 
+                            tipo="CASO 1";
+                        if (e=="2") 
+                            tipo="CASO 2";
+                        if (e=="3") 
+                            tipo="CASO 3";
+                        if (e=="4") 
+                            tipo="CASO 4";
+                        if (e=="5") 
+                            tipo="CASO 5";
+                        return tipo
+                        }
+                },
+                {
+                    title:"ACCION",
+                    orderable: false,
+                    searchable:false,
+                    width:"auto",
+                    render: function (data, type, row, meta) {
+                        var e=row['idEstadoComprobante'];
+                        var opcion=0;
+                        if(e==null)
+                            opcion = 0;
+                        else{
+                            if (e=="0") 
+                                return '<button type="button" class="btn btn-xs btn-default">CASO 0</button>';
+                            if (e=="1") 
+                                return '<button type="button" class="btn btn-xs btn-primary">CASO 1</button>';
+                            if (e=="2") 
+                                return '<button type="button" class="btn btn-xs btn-success">CASO 2</button>';
+                            if (e=="3") 
+                                return '<button type="button" class="btn btn-xs btn-info">CASO 3</button>';
+                            if (e=="4") 
+                                return '<button type="button" class="btn btn-xs btn-warning">CASO 4</button>';
+                            if (e=="5") 
+                                return '<button type="button" class="btn btn-xs btn-danger">CASO 5</button>';
+                            }
+                        }
                 }
             ]
         });
