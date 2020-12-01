38966b592a729e8c55c55a4e41bd107cfe93519e
Carlos Chacón <carlos.echc11@gmail.com>
2020-12-01 13:31:00 -0600

    Update xmlTE.php

--- class/xmlTE.php
+++ class/xmlTE.php
@@ -78,6 +78,7 @@ class xmlTE{
         foreach($transaccion->detalleFactura as $d){
             $xmlString .= '<LineaDetalle>
                       <NumeroLinea>' . $l . '</NumeroLinea>
+                      <Codigo>2227000000200</Codigo>
                       <Cantidad>' . $d->cantidad . '</Cantidad>
                       <UnidadMedida>' . self::getUnidadMedidaCod($d->idUnidadMedida) . '</UnidadMedida>';
             if (!empty($d->detalle))
