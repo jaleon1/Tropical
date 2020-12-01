bd37b9f4bad7bbefbd5d498d37b41f8a61aa3914
Carlos Chacón <carlos.echc11@gmail.com>
2020-12-01 13:30:20 -0600

    Update xmlNC.php

--- class/xmlNC.php
+++ class/xmlNC.php
@@ -134,6 +134,7 @@ class xmlNC{
         foreach($transaccion->detalleFactura as $d){
             $xmlString .= '<LineaDetalle>
                       <NumeroLinea>' . $l . '</NumeroLinea>
+                      <Codigo>2227000000200</Codigo>
                       <Cantidad>' . $d->cantidad . '</Cantidad>
                       <UnidadMedida>' . self::getUnidadMedidaCod($d->idUnidadMedida) . '</UnidadMedida>';
             if (!empty($d->detalle))
